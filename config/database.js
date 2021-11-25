7c00642f15184c34a40a2b789af90bfe2d9517a8
TieuMinhLapTrinh <minhreuspro123@gmail.com>
2021-11-26 00:07:18 +0700

    first22

---
+++ config/database.js
@@ -0,0 +1,25 @@
+
+//module.exports = {
+//	'url': 'mongodb://m99cm.herokuapp.com:27017',
+//	'options': {
+//		'user':   'admin',
+//		'pass':   'YouPassforUser',
+//		'dbName': 'PlayGameChuan2',
+//		'useNewUrlParser': true,
+//		'useUnifiedTopology': true,
+//		//'autoIndex':       false,
+//	},
+//};
+
+
+module.exports = {
+	'url': 'mongodb://m99cm.herokuapp.com:27017',
+	'options': {
+	//	'user':   'admin',
+	//	'pass':   'Ddata@123456',
+		'dbName': 'PlayGame', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
